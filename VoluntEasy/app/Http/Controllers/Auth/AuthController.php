ae4d4975a6d19847bd2725381709123a701b2a22
xstrtok <sweepy1982@gmail.com>
2015-05-27 14:02:11 +0300

    Handle redirection for user login.
    Login from <address>/auth/login redirects to <address>/main

--- VoluntEasy/app/Http/Controllers/Auth/AuthController.php
+++ VoluntEasy/app/Http/Controllers/Auth/AuthController.php
@@ -20,6 +20,8 @@ class AuthController extends Controller {
 
 	use AuthenticatesAndRegistersUsers;
 
+	protected $redirectPath = '/main/';
+
 	/**
 	 * Create a new authentication controller instance.
 	 *
