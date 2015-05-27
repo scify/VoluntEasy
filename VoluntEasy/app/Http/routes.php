1bb7867a9ab776cb65a6d5edd07577a8b5a3352b
xstrtok <sweepy1982@gmail.com>
2015-05-27 13:56:55 +0300

    Added app/Http/routes.php
    Missed from previous commit.

--- VoluntEasy/app/Http/routes.php
+++ VoluntEasy/app/Http/routes.php
@@ -3,3 +3,8 @@
 Route::get('main','IndexPage@mainIndex');
 
 Route::get('main/orgUnits/unitEntry','MenuController@a1');
+
+Route::controllers([
+	'auth' => 'Auth\AuthController',
+	'password' => 'Auth\PasswordController',
+]);
