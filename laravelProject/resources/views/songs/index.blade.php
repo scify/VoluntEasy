42b17c99215d7fc569ca0142ecd4db7f6a75fdba
arix <aris.stru@gmail.com>
2015-06-01 12:34:08 +0300

    clean project

--- laravelProject/resources/views/songs/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('master')
-
-@section('bodyContent')
-    <h1>List of the songs</h1>
-    @foreach($songs as $index => $song)
-        <li><a href="/songs/{{$index}}"> {{$song}} </a></li>
-    @endforeach
-@stop
--- laravelProject/resources/views/songs/show.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('master')
-
-@section('bodyContent')
-    <h1>List of the songs</h1>
-    <h2> {{ $song }} </h2>
-@stop
