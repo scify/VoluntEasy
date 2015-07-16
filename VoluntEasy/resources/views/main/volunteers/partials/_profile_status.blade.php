141636d6ef55c1eb6cdf2eef51a317a1052bdce0
xristv <xrist.ven@gmail.com>
2015-07-16 17:30:28 +0300

    Some work on volunteer profile

---
+++ VoluntEasy/resources/views/main/volunteers/partials/_profile_status.blade.php
@@ -0,0 +1,57 @@
+<div class="row">
+    <div class="col-12">
+        <div class="panel panel-default smallHeading">
+            <div class="panel-heading ">
+                <h3 class="panel-title">Κατάσταση Εθελοντή</h3>
+            </div>
+            <div class="panel-body">
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>Κατάσταση</th>
+                        <th>Οργανωτική Μονάδα</th>
+                        <th>Ημ. Ισχύος</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Ενεργός</td>
+                        <td>Μπλα μπλα</td>
+                        <td>Λαλαλα</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="row">
+    <div class="col-12">
+        <div class="panel panel-default smallHeading">
+            <div class="panel-heading ">
+                <h3 class="panel-title">Εκκρεμότητες</h3>
+            </div>
+            <div class="panel-body">
+                <table class="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>Κατάσταση</th>
+                        <th>Οργανωτική Μονάδα</th>
+                        <th>Ημ. Ισχύος</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Ενεργός</td>
+                        <td>Μπλα μπλα</td>
+                        <td>Λαλαλα</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
