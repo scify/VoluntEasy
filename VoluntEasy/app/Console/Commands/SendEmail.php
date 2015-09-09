f09fa376959076a19f0b8a938e9a28053ea0e5f4
xstrtok <sweepy1982@gmail.com>
2015-09-09 12:08:40 +0300

    Refactored fixed sending email to volunteers.
    If an action is set to expire in 7 days, send an email to all volunteers
    containing the questionnaire link.

--- VoluntEasy/app/Console/Commands/SendEmail.php
+++ VoluntEasy/app/Console/Commands/SendEmail.php
@@ -54,51 +54,33 @@ class SendEmail extends Command {
         $week_check = Carbon::now()->addDays(7)->format('Y-m-d');
         $date_now = Carbon::now()->format('Y-m-d');
 
-        /* TODO: Bring check 'if action ends in 7 days' up to be evaluate first. */
-        /* TODO: Use `continue;` to check if action->end_date < date(now) */
         foreach($end_dates as $action_end_date) {
             /* Find if an action end_date has already passed. If yes, do nothing.
              * If not, check if action ends in 7 days to fire mail questionnaires
              * to volunteers */
             if ($date_now > $action_end_date->end_date) {
-                /* debug msg. */
-                echo $action_end_date->description . " found a past action\n";
                 continue;
             } else {
                 if (strcmp($action_end_date->end_date, $week_check) === 0) {
-                    /* debug msg. TODO: remove. */
-                    echo $action_end_date->end_date . " dates match for action " . $action_end_date->description ."\n";
-                    echo "\nVOLUNTEERS ARE: " . "\n";
-                    /* Grab the questionnaire link and insert into $data array. */
-                    $data = array('questionnaire' => $action_end_date->questionnaire_action_link);
-                    \Mail::queue('emails.test', $data, function($message)
+                    /* Join actions_volunteers with volunteers to check volunteers that belong to expiring action. */
+                    $mailqueue= \DB::table('actions_volunteers')->join('volunteers', 'actions_volunteers.id', '=', 'volunteers.id')->get();
+                    foreach($mailqueue as $notify_users)
                     {
-                        /* Join actions_volunteers with volunteers to check volunteers that belong to action. */
-                        $mailqueue= \DB::table('actions_volunteers')->join('volunteers', 'actions_volunteers.id', '=', 'volunteers.id')->get();
-                        $message->from('test@scify.org', 'Mr Test');
-                        foreach($mailqueue as $notify_users)
+                        /* Grab all things needed to pass to email view. */
+                        $data = array(
+                            'questionnaire' => $action_end_date->questionnaire_action_link,
+                            'username' => $notify_users->name,
+                            'usermail' => $notify_users->email
+                        );
+                        \Mail::queue('emails.test', $data, function($message) use ($data)
                         {
-                            $message->to($notify_users->email)->subject('Test subject');
-                        }
-                    });
-                    /* Raw Mail testing. */
-//                        \Mail::raw('This is text', function($message)
-//                        {
-//                            $mailqueue= \DB::table('actions_volunteers')->join('volunteers', 'actions_volunteers.id', '=', 'volunteers.id')->get();
-//                            $message->from('test@scify.org', 'ACK');
-//                            foreach($mailqueue as $notify_volunteers)
-//                            {
-//                            $message->to($notify_volunteers->email)->subject('email test');
-//                            }
-//                        });
-
-//                        echo "  " . $notify_volunteers->email . "\n";
-                    // Get all volunteers mail from action
+                            $message->from('test@scify.org', 'Me Test');
+                            $message->to($data['usermail'])->subject('Testing subject');
+                        });
+                    }
 
                 } else {
-                    /* debug msg. TODO: remove. */
-                    echo $action_end_date->end_date . " dates do not match for action " . $action_end_date->description . "\n";
-                    /* continue; */
+                    continue;
                 }
             }
         }
