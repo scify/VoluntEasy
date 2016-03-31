fab73d77f4064e2ceba3a67c2c8f4f1091230e85
xristv <xrist.ven@gmail.com>
2016-03-31 10:43:13 +0300

    Debugging email on server

--- VoluntEasy/app/Http/routes.php
+++ VoluntEasy/app/Http/routes.php
@@ -134,7 +134,7 @@ Route::get('steps/volunteer/{id}', 'StepController@volunteerSteps');
 //Rating Routes //
 ////////////////
 Route::post('ratings/action/store', 'RatingController@storeActionRating');
-Route::post('ratings/action/volunteers/store', 'RatingController@storeVolunteersRating');
+Route::get('ratings/action/volunteers/store', 'RatingController@storeVolunteersRating');
 Route::get('ratings/action/thankyou/{actionId}', 'RatingController@actionThankyou');
 Route::get('ratings/action/volunteers/thankyou/{actionId}', 'RatingController@volunteersThankyou');
 Route::get('ratings/action/{token}', 'RatingController@rateAction');
